80b4e006cb5ea9914e62e50cddf136efaac8bf46
kevinkaitung <dadado0570@gmail.com>
2021-03-27 01:24:43 +0800

    first commit
    專題遊戲第一次上傳github，

---
+++ Computer Project Game Practice/Assets/Scripts/gameLogicController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class gameLogicController : MonoBehaviourPunCallbacks
+{
+    //gameLogicController Singleton
+    //Only create NodeManager once
+    private static gameLogicController s_Instance = null;
+    public static gameLogicController instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                s_Instance = FindObjectOfType(typeof(gameLogicController)) as gameLogicController;
+
+                if (s_Instance == null)
+                    Debug.Log("Could not locate a gameLogicController " +
+                              "object. \n You have to have exactly " +
+                              "one gameLogicController in the scene.");
+            }
+            return s_Instance;
+        }
+    }
+
+
+    //public bool[] isDoneInThisStage;
+    //how many point to put in this stage
+    private int thisStageCount;
+    //how many points have been clicked by player (already build that point)
+    private int thisStagePutCount;
+    //current stage
+    private int currentStageNumber;
+    //game data nodes' length
+    private int dataNodeLen;
+    //index for access game data nodes
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStageNumber = 1;
+        index = 0;
+        dataNodeLen = nodeManager.instance.dataRoot.gameDataNodes.Length;
+        //setting first stage
+        settingStage(currentStageNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void settingStage(int stageNumber)
+    {
+        thisStageCount = 0;
+        thisStagePutCount = 0;
+        if (index < dataNodeLen)
+        {
+            //put notice points of this stage
+            while (nodeManager.instance.dataRoot.gameDataNodes[index].stage == stageNumber)
+            {
+                GameObject clone = PhotonNetwork.Instantiate("noticePoint", nodeManager.instance.dataRoot.gameDataNodes[index].position, Quaternion.identity);
+                noticePoint tmp = clone.GetComponent<noticePoint>();
+                tmp.pos = nodeManager.instance.dataRoot.gameDataNodes[index].position;
+                tmp.rot = nodeManager.instance.dataRoot.gameDataNodes[index].rotation;
+                tmp.sca = nodeManager.instance.dataRoot.gameDataNodes[index].scale;
+                tmp.objShap = nodeManager.instance.dataRoot.gameDataNodes[index].objShape;
+                tmp.materialNam = nodeManager.instance.dataRoot.gameDataNodes[index].materialName;
+                tmp.stag = nodeManager.instance.dataRoot.gameDataNodes[index].stage;
+                index++;
+                thisStageCount++;
+                if(index == dataNodeLen)
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Game Finish!!!");
+        }
+    }
+
+    public void playerPutThingsOnPoint(noticePoint pointInfo, string handyMaterial)
+    {
+        thisStagePutCount++;
+        //Put right game object on the player clicked point
+        PhotonNetwork.Instantiate(pointInfo.objShap, pointInfo.pos, Quaternion.Euler(pointInfo.rot));
+        //if all notice points have been clicked (put), go next stage
+        if (thisStagePutCount == thisStageCount)
+        {
+            currentStageNumber++;
+            Debug.Log("go next stage: " + currentStageNumber);
+            settingStage(currentStageNumber);
+        }
+    }
+}
---
+++ Computer Project Game Practice/Assets/Scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public string holdMaterial;
+    private Camera playerCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get player's camera
+        playerCam = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKey("up")) {  transform.Translate( 0, 0, 0.1f );  }
+        // 按住 上鍵 時，物件每個 frame 朝自身 z 軸方向移動 0.1 公尺
+
+        if ( Input.GetKey("down")) {  transform.Translate( 0, 0, -0.1f );  }
+        // 按住 下鍵 時，物件每個 frame 朝自身 z 軸方向移動 -0.1 公尺
+
+        if ( Input.GetKey("left")) {  transform.Rotate( 0, -3, 0 );  }
+        // 按住 左鍵 時，物件每個 frame 以自身 y 軸為軸心旋轉 -3 度
+
+        if ( Input.GetKey("right")) {  transform.Rotate( 0, 3, 0 );  }
+        // 按住 右鍵 時，物件每個 frame 以自身 y 軸為軸心旋轉 3 度
+        
+        Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.transform.name);
+                if(hit.collider.tag == "noticePoint")
+                {
+                    Debug.Log("click notice point");
+                    //Get player clicked point's component to know clicked point's detail
+                    noticePoint clickedPointInfo = hit.collider.gameObject.GetComponent<noticePoint>();
+                    //Pass detail of clicked point and player's hold material to game controller
+                    gameLogicController.instance.playerPutThingsOnPoint(clickedPointInfo, holdMaterial);
+                }
+            }
+        }
+    }
+}
