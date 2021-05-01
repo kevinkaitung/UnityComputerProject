88c0a285fa1ea03cb9b63a826a704b57e2b089ab
kevinkaitung <dadado0570@gmail.com>
2021-05-01 15:25:23 +0800

    basic game logic
    基礎關大部分功能完成

---
+++ Computer Project Game Practice_clone_0/Assets/Scripts/gameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace mySection
+{
+    public class gameManager : MonoBehaviourPunCallbacks
+    {
+        [Tooltip("Prefab- 玩家的角色")]
+        public GameObject playerPrefab;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("playerPrefab 遺失, 請在 Game Manager 重新設定", this);
+            }
+            else
+            {
+                //Debug.LogFormat("動態生成玩家角色 {0}", Application.loadedLevelName);
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0,2,0), Quaternion.identity, 0);
+            }
+        }
+
+        //新玩家進入時，呼叫OnPlayerEnteredRoom
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("{0} 進入遊戲室", other.NickName);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
