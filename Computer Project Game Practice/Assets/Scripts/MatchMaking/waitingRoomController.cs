afa8c1d574f4c87deea962437d4ee4e7ecc4f04d
kevinkaitung <dadado0570@gmail.com>
2021-06-11 00:50:38 +0800

    team assign

--- Computer Project Game Practice/Assets/Scripts/MatchMaking/waitingRoomController.cs
+++ Computer Project Game Practice/Assets/Scripts/MatchMaking/waitingRoomController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using Photon.Pun.UtilityScripts;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class waitingRoomController : MonoBehaviourPunCallbacks
 {
@@ -35,7 +37,22 @@ public class waitingRoomController : MonoBehaviourPunCallbacks
         {
             GameObject tempListing = Instantiate(playerListingPrefab, playersContainer);
             Text tempText = tempListing.transform.GetChild(0).GetComponent<Text>();
+            //get playerListing component - Image to change color to its team color
+            Image tempImage = tempListing.GetComponent<Image>();
             tempText.text = player.NickName;
+            //try get player's team
+            if (player.GetPhotonTeam() != null)
+            {
+                //set playerListing to team color
+                if (player.GetPhotonTeam().Name == "Blue")
+                {
+                    tempImage.color = Color.blue;
+                }
+                else
+                {
+                    tempImage.color = Color.red;
+                }
+            }
         }
     }
 
@@ -52,6 +69,44 @@ public class waitingRoomController : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsMasterClient)
         {
             startButton.SetActive(true);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //after join room successfully, do some initialization
+        roomNameDisplay.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+        //only master client can start the game
+        if (PhotonNetwork.IsMasterClient)
+        {
+            startButton.SetActive(true);
+        }
+        else
+        {
+            startButton.SetActive(false);
+        }
+        //re-display current room player list
+        ClearPlayerListings();
+        ListPlayers();
+        
+        //already in room, and player joins team
+        PhotonTeam[] availableTeam = PhotonTeamsManager.Instance.GetAvailableTeams();
+        int[] teamMembersCount = new int[2];
+        teamMembersCount[0] = PhotonTeamsManager.Instance.GetTeamMembersCount(availableTeam[0]);
+        teamMembersCount[1] = PhotonTeamsManager.Instance.GetTeamMembersCount(availableTeam[1]);
+        Debug.Log("Blue:" + teamMembersCount[0]);
+        Debug.Log("Red:" + teamMembersCount[1]);
+        if(teamMembersCount[0] > teamMembersCount[1])
+        {
+            Debug.Log("joined?"+PhotonNetwork.LocalPlayer.JoinTeam(teamCode:2));    //team code begin from 1
+        }
+        else if(teamMembersCount[0] < teamMembersCount[1])
+        {
+            Debug.Log("joined?"+PhotonNetwork.LocalPlayer.JoinTeam(teamCode:1));
+        }
+        else
+        {
+            Debug.Log("joined?"+PhotonNetwork.LocalPlayer.JoinTeam(teamCode:(byte)Random.Range(1,3)));
         }
     }
 
@@ -73,6 +128,10 @@ public class waitingRoomController : MonoBehaviourPunCallbacks
 
     public void BackOnClick()
     {
+        //local player leave team
+        Debug.Log("leave?"+PhotonNetwork.LocalPlayer.LeaveCurrentTeam());
+        //callback function be called by other clients to update playerListings in container
+        //need to leave team first, then leave room. otherwise, leave team fail
         PhotonNetwork.LeaveRoom();
         PhotonNetwork.LeaveLobby();
         StartCoroutine(rejoinLobby());
@@ -81,24 +140,20 @@ public class waitingRoomController : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        roomNameDisplay.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
-        //only master client can start the game
-        if (PhotonNetwork.IsMasterClient)
-        {
-            startButton.SetActive(true);
-        }
-        else
-        {
-            startButton.SetActive(false);
-        }
-        //re-display current room player list
-        ClearPlayerListings();
-        ListPlayers();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        //after let player call JointTeam, must wait server actually set custom properties and broadcast to all client
+        //after finishing setting team, update the playerListings' color in container
+        ClearPlayerListings();
+        ListPlayers();
     }
 }
