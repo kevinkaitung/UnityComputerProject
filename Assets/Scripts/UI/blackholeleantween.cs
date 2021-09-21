857ac1222831b69642a9f0e01f1341466f257b8e
p89108989 <p881021@gmail.com>
2021-09-22 06:03:22 +0800

    UI更新

---
+++ Assets/Scripts/UI/blackholeleantween.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class blackholeleantween : MonoBehaviour
+{
+    public GameObject blackhole; 
+    int i = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackhole.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        blackhole.transform.rotation = Quaternion.Euler(0.0f, 0.0f, i);
+        i+=i;
+    }
+
+    
+    
+}
