5d517cb7fd7f804c924f3f4603af48976c9a29e3
blacksugar890424 <74858894+blacksugar890424@users.noreply.github.com>
2021-10-24 22:40:29 +0800

    test voice

--- Assets/Scripts/MatchMaking/waitingRoom/voiceController.cs
+++ Assets/Scripts/MatchMaking/waitingRoom/voiceController.cs
@@ -9,17 +9,24 @@ using TMPro;
 using UnityEngine.UI;
 using Photon.Realtime;
 using Photon.Pun.UtilityScripts;
+using Photon.Voice;
 
-public class voiceController : MonoBehaviour
+public class voiceController : MonoBehaviourPunCallbacks
 {
     public GameObject voice;
     public GameObject muteBtn;
     bool isMute = false;
+    // when flag == 0, it's red team
+    // when flag == 1, it's blue team
+    int flag = 0;
+    // when index == 1, it's teamMsg
+    // when index == 0, it's worldMsg
+    int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -44,6 +51,61 @@ public class voiceController : MonoBehaviour
             mButton.image.sprite = Resources.Load<Sprite>("mute");
             voice.SetActive(false);
             isMute = !isMute;
+        }
+    }
+
+    // changeVoiceChannel
+    public void changeVoiceChannel()
+    {
+        // get team
+        // when flag == 0, it's red team
+        // when flag == 1, it's blue team
+        object tmp;
+        PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("_pt", out tmp);
+        if((byte)tmp == 1)
+        {
+            flag = 1;
+        }
+        else
+        {
+            flag = 0;
+        }
+        // when index == 1, it's teamMsg
+        // when index == 0, it's worldMsg
+        // worldGroup = 0;
+        // redGroup = 1;
+        // blueGroup = 2;
+        if(flag == 0)
+        {
+            index = (index + 1) % 2;
+            if(index == 1)
+            {
+                //Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.OpChangeGroups(worldGroup, redGroup);
+                Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.GlobalAudioGroup = 1;
+                Debug.Log("切換到紅隊語音頻道");
+            }
+            else
+            {
+                //Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.OpChangeGroups(redGroup, worldGroup);
+                Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.GlobalAudioGroup = 0;
+                Debug.Log("切換到世界語音頻道");
+            }
+        }
+        else
+        {
+            index = (index + 1) % 2;
+            if(index == 1)
+            {
+                //Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.OpChangeGroups(worldGroup, blueGroup);
+                Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.GlobalAudioGroup = 2;
+                Debug.Log("切換到藍隊語音頻道");
+            }
+            else
+            {
+                //Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.OpChangeGroups(blueGroup, worldGroup);
+                Photon.Voice.PUN.PhotonVoiceNetwork.Instance.Client.GlobalAudioGroup = 0;
+                Debug.Log("切換到世界語音頻道");
+            }
         }
     }
 }
